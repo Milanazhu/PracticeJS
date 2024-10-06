18d0f672e958fe41a3b709b3c40a5afa6d8a40c0
Milanocka- <matildarumc@gmail.com>
2024-10-06 14:21:23 +0300

    Message

---
+++ Modul 5/modul.js
@@ -0,0 +1,6 @@
+const contetntEl = document.querySelector('.contetnt');
+const h1El = document.createElement('h1');
+const header = document.createElement('header');
+
+contetntEl.append(h1El);
+contetntEl.prepend(header);
